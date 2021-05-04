9461221b77f8c493541aa65ce2d2168e396e5427
Alejandro M. Coca <alxmcr@outlook.com>
2021-05-04 01:55:48 -0400

    Refactor and Improvement in the Banner

--- src/components/BannerNominations/index.jsx
+++ src/components/BannerNominations/index.jsx
@@ -2,20 +2,26 @@ import { faMedal } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import styles from "./BannerNominations.module.css"
 
-export function BannerNominations({ maxNominations = 5 }) {
+export function BannerNominations({ maxNominations = 5, setShowBanner }) {
+    const closeBanner = () => { setShowBanner(false) }
     return (
-        <div className={styles.banner}>
-            <i className={styles.bannerIcon}>
-                <FontAwesomeIcon icon={faMedal} />
-            </i>
-            <div>
-                <h2 className={styles.bannerTitle}>Congratulations!</h2>
-                <p className={styles.bannerMessage}>
-                    You have already nominated <span className={styles.bannerEmphasis}>{` ${maxNominations} movies`}</span>.
-                </p>
-                <p className={styles.bannerMessage}>
+        <div className={styles.banner} role="banner">
+            <div className={styles.bannerText}>
+                <i className={styles.bannerIcon}>
+                    <FontAwesomeIcon icon={faMedal} />
+                </i>
+                <p className="bannerMessage">
+                    You have already nominated <span className={styles.bannerEmphasis}>{`${maxNominations} movies`} </span>.
+                    <br />
                     Thank you so much for your support and help!
                 </p>
+            </div>
+            <div className={styles.bannerActions}>
+                <button
+                    className={styles.bannerButton}
+                    onClick={closeBanner}>
+                    CLOSE
+                </button>
             </div>
         </div>
     )
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { BannerNominations } from "components/BannerNominations";
 import { HeroApp } from "components/HeroApp";
 import { MoviesFiltered } from "sections/MoviesFiltered";
@@ -14,24 +14,31 @@ import styles from './HomePage.module.css'
 export function HomePage() {
     const maxNominations = 5;
     const [title, setTitle] = useState("");
+    const [showBanner, setShowBanner] = useState(false);
     const [reducer, dispatch] = useReducer(moviesReducer, initialMoviesReducer)
     const moviesNominated = reducer.moviesNominated;
     const valueProvider = {
         moviesNominated,
         dispatch
     }
+    useEffect(() => {
+        setShowBanner(moviesNominated.length >= maxNominations);
+    }, [moviesNominated, maxNominations]);
     return (
         <MoviesNominatedContext.Provider value={valueProvider}>
             <main className={styles.home}>
+                {showBanner &&
+                    <BannerNominations
+                        maxNominations={maxNominations}
+                        setShowBanner={setShowBanner}
+                    />
+                }
                 <HeroApp />
                 {moviesNominated.length < maxNominations
                     && <SearchMovieForm title={title} setTitle={setTitle} />
                 }
                 {moviesNominated.length < maxNominations
                     && <MoviesFiltered title={title} />
-                }
-                {moviesNominated.length >= maxNominations &&
-                    <BannerNominations maxNominations={maxNominations} />
                 }
                 <MoviesNominated maxNominations={maxNominations} />
             </main>
